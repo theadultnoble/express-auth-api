aad1a051b9a5ee87c0d0353861a1ffb40c9e1068
Noble Okafor <okafornoble202@gmail.com>
2023-05-17 02:36:22 +0100

    renamed env file and updated jwt logic

---
+++ middlewares/requireAuth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+require("dotenv").config(); // Load the .env file
+
+module.exports = (req, res, next) => {
+  const JWT_SECRET = process.env.JWT_SECRET;
+
+  //Extracting token from authorization header
+  const { authorization } = req.headers;
+  //authorization === 'Bearer "token"'
+  if (!authorization) {
+    return res.status(404).send({ error: "must be logged in" });
+  }
+  const token = authorization.replace("Bearer ", "");
+
+  //Verifying if the token is valid.
+  jwt.verify(token, JWT_SECRET, async (err, payload) => {
+    if (err) {
+      console.log("hello");
+      return res.status(403).send("Could not verify token");
+    }
+
+    const { userID } = payload;
+    const user = await User.findById(userID);
+    req.user = user;
+  });
+  next();
+};
